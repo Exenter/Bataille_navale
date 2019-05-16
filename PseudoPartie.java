6858de78c2bbb30daa2305dae6f348f6b7d4330c
pFox29 <49549937+pFox29@users.noreply.github.com>
2019-05-16 17:04:56 +0200

    Add files via upload

--- PseudoPartie.java
+++ PseudoPartie.java
@@ -5,78 +5,89 @@ public class PseudoPartie {
 		// TODO Auto-generated method stub
 		Carte c = new Carte();
 		
-		Point b1_p1 = new Point(3,2);
-		Point b1_c = new Point(3,3);
-		Point b1_p2 = new Point(3,4);
-		Bateaux b1 = new Bateaux(b1_p1, b1_c, b1_p2, Proprio.Humain);
-		c.addBateaux(b1);
 		
-		Point b2_p1 = new Point(12,5);
-		Point b2_c = new Point(13,5);
-		Point b2_p2 = new Point(14,5);
-		Bateaux b2 = new Bateaux(b2_p1, b2_c, b2_p2, Proprio.Humain);
-		c.addBateaux(b2);
+		Joueur Hsapien = new Joueur(Proprio.Humain, 2,"Jack_le_brave");
+		Joueur IA = new Joueur(Proprio.Machine,2,"BarbeBleu");
 		
-		Point b3_p1 = new Point(12,8);
-		Point b3_c = new Point(12,9);
-		Point b3_p2 = new Point(12,10);
-		Bateaux b3 = new Bateaux(b3_p1, b3_c, b3_p2, Proprio.Machine);
-		c.addBateaux(b3);
+		InterfaceGraphique IG = new InterfaceGraphique(Hsapien, IA); 
+		IG.les_boutons();
 		
-		Point b4_p1 = new Point(5,7);
-		Point b4_c = new Point(6,7);
-		Point b4_p2 = new Point(7,7);
-		Bateaux b4 = new Bateaux(b4_p1, b4_c, b4_p2, Proprio.Machine);
-		c.addBateaux(b4);
+		for(Bateaux b:Hsapien.listeBat) {
+			c.addBateaux(b);
+			IG.setCaseBateau(b);
+		}
 		
+		for(Bateaux bm:IA.listeBat) {
+			c.addBateaux(bm);
+			IG.setCaseBateau(bm);
+		}
+		
+		
+		
+		Point p = IA.choixAleaPointTire();
 		//System.out.println(c.verifDepPossible(b4, Sens.Avant));
-		System.out.println("AVANT DEP");
-		for(int i=b1.p1.x; i<=b1.p2.x; i++) {
-			for(int j=b1.p1.y; j<=b1.p2.y; j++) {
-				System.out.println("i: "+i+" j: "+j);
-				System.out.println(c.cases[i][j].occupant);
-				System.out.println(c.cases[i][j].vision);
-				//System.out.println("\n");
-			}
-		}
+		System.out.println("AVANT tire:");
+		System.out.println("etat case: "+ c.cases[p.x][p.y].vision+"  " +c.cases[p.x][p.y].occupant);
+//		System.out.println("vie de b4: "+b4.vie);
+//		System.out.println("vie de b4 dans la liste bateau: "+c.listeBatMachine.get(1).vie);
+		System.out.println("score du joueur: "+ IA.score);
+//		System.out.println("vie bat de la case: " + c.cases[7][7].bat.vie);
 		
-		Sens s = Sens.Rotation;
-		
-		if(c.verifDepPossible(b1, s)) {
-			//System.out.println("eh beh");
-			b1.deplacementBateau(s);
-			//System.out.println("orientaion: "+b4.ori);
-			//System.out.println(c.cases[b4.p1.x-1][b4.p1.y].occupant);
-			c.updateCaseBateau(b1,s);
-			//System.out.println("orientaion: "+b4.ori);
-			//System.out.println(c.cases[b4.p1.x-1][b4.p1.y].occupant);
-			c.updateVisibilité(b1, s);
-		};
-	
-		System.out.println("\n //////// APRES DEP ///////////");
-		System.out.println("\n devrait etre libre et claire");
-		int i =3;
-		int j =2; 
-		System.out.println("i: "+i+" j: "+j);
-		System.out.println(c.cases[i][j].occupant);
-		System.out.println(c.cases[i][j].vision);
-		
-		//System.out.println("\n devrait etre claire");
-		i =3;
-		j =4; 
-		System.out.println("i: "+i+" j: "+j);
-		System.out.println(c.cases[i][j].occupant);
-		System.out.println(c.cases[i][j].vision);
-		
-		System.out.println("\n devrait etre Humain");
-		for( i=b1.p1.x; i<=b1.p2.x; i++) {
-			for( j=b1.p1.y; j<=b1.p2.y; j++) {
-				System.out.println("i: "+i+" j: "+j);
-				System.out.println(c.cases[i][j].occupant);
-				System.out.println(c.cases[i][j].vision);
-				//System.out.println("\n");
-			}
-		}
+		System.out.println("Apres tire:");
+		c.tire(p.x, p.y, IA);
+		System.out.println("etat case: "+ c.cases[p.x][p.y].vision+"  "+c.cases[p.x][p.y].occupant);
+//		System.out.println("vie de b4: "+b4.vie);
+//		System.out.println("vie de b4 dans la liste bateau: "+c.listeBatMachine.get(1).vie);
+		System.out.println("score du joueur: "+ IA.score);
+//		System.out.println("vie bat de la case: " + c.cases[7][7].bat.vie);
+//		
+//		for(int i=b1.p1.x; i<=b1.p2.x; i++) {
+//			for(int j=b1.p1.y; j<=b1.p2.y; j++) {
+//				System.out.println("i: "+i+" j: "+j);
+//				System.out.println(c.cases[i][j].occupant);
+//				System.out.println(c.cases[i][j].vision);
+//				//System.out.println("\n");
+//			}
+//		}
+//		
+//		Sens s = Sens.Avant;
+//		
+//		if(c.verifDepPossible(b1, s)) {
+//			//System.out.println("eh beh");
+//			b1.deplacementBateau(s);
+//			//System.out.println("orientaion: "+b4.ori);
+//			//System.out.println(c.cases[b4.p1.x-1][b4.p1.y].occupant);
+//			c.updateCaseBateau(b1,s);
+//			//System.out.println("orientaion: "+b4.ori);
+//			//System.out.println(c.cases[b4.p1.x-1][b4.p1.y].occupant);
+//			c.updateVisibilité(b1, s);
+//			
+//		};
+//	
+//		System.out.println("\n //////// APRES DEP ///////////");
+//		System.out.println("\n devrait etre libre et claire");
+//		int i =3;
+//		int j =2; 
+//		System.out.println("i: "+i+" j: "+j);
+//		System.out.println(c.cases[i][j].occupant);
+//		System.out.println(c.cases[i][j].vision);
+//		
+//		//System.out.println("\n devrait etre claire");
+//		i =3;
+//		j =4; 
+//		System.out.println("i: "+i+" j: "+j);
+//		System.out.println(c.cases[i][j].occupant);
+//		System.out.println(c.cases[i][j].vision);
+//		
+//		System.out.println("\n devrait etre Humain");
+//		for( i=b1.p1.x; i<=b1.p2.x; i++) {
+//			for( j=b1.p1.y; j<=b1.p2.y; j++) {
+//				System.out.println("i: "+i+" j: "+j);
+//				System.out.println(c.cases[i][j].occupant);
+//				System.out.println(c.cases[i][j].vision);
+//				//System.out.println("\n");
+//			}
+//		}
 		
 
 		
